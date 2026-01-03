16eeeb6047517e5aee0e7c0a6acab52e1ecd52e6
Rowan Border <rowanjborder@gmail.com>
2026-01-03 16:33:38 +0200

    update setup script

--- setup-bluebird.sh
+++ setup-bluebird.sh
@@ -5,6 +5,7 @@ sudo apt install -y python3-picamera2
 sudo apt install -y python3-opencv
 sudo apt install -y libsndfile1
 
+python3 -m pip config set global.break-system-packages true
 
 pip install -U piper-tts sounddevice numpy
 pip install -U transformers pillow torch
@@ -15,3 +16,12 @@ huggingface-cli download dennisjooo/Birds-Classifier-EfficientNetB2 \
   --local-dir /home/pi/models/Birds-Classifier-EfficientNetB2 \
   --local-dir-use-symlinks False
 
+sudo sh -c 'echo "camera_auto_detect=0" >> /boot/firmware/config.txt'
+sudo sh -c 'echo "dtoverlay=imx708,cam0" >> /boot/firmware/config.txt'
+sudo sh -c 'echo "dtoverlay=imx708,cam1" >> /boot/firmware/config.txt'
+
+./install-service.sh
+
+echo "Setup complete! Rebooting your Raspberry Pi to apply camera settings."
+
+sudo reboot
